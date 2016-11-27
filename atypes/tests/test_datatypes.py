193332a9ba2341dd38d2659e453045a53fd207cc
Javis Sullivan <javissullivan@gmail.com>
2016-11-27 08:42:50 -0500

    new: add tests for Stack type

--- atypes/tests/test_datatypes.py
+++ atypes/tests/test_datatypes.py
@@ -8,15 +8,43 @@ from atypes import Stack
 class TestStackMethods(unittest.TestCase):
 
     def setUp(self):
-        self.first_list = [1, 2, 3, 4, 5]
+        pass
 
     def tearDown(self):
         pass
 
     def test_clear(self):
-        stack = Stack(self.first_list)
-        print(stack)
-        self.assertEqual(True, True)
+        stack = Stack(*[x for x in range(1, 6)])
+        self.assertEqual(stack.length, 5)
+        stack.clear()
+        self.assertEqual(stack.length, 0)
+
+    def test_peek(self):
+        stack = Stack()
+        stack.push(35)
+        self.assertEqual(stack.peek(), 35)
+
+    def test_pull(self):
+        stack = Stack(*[x for x in range(1, 6)])
+        old_length = stack.length
+        self.assertEqual(stack.pull(), 5)
+        self.assertLess(stack.length, old_length)
+
+    def test_push(self):
+        stack = Stack()
+        stack.push(77)
+        self.assertEqual(stack.peek(), 77)
+
+    def test_stack_length(self):
+        stack = Stack()
+        stack.push(6)
+        self.assertEqual(stack.length, 1)
+
+    def test_stack_loading(self):
+        stack = Stack()
+        stack.push(*[x for x in range(1, 101)])
+        self.assertEqual(stack.length, 100)
+        self.assertEqual(stack.pull(), 100)
 
 
 if __name__ == '__main__':
